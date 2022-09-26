c474dba9e33cf115a1575baaf99c6f0629f12f50
GoRuGoo <dachanyr8989@gmail.com>
2022-09-26 17:28:29 +0900

    cv2.imshowに対応させた

--- add_alpha_channel.py
+++ add_alpha_channel.py
@@ -1,3 +1,4 @@
+from pickletools import uint1, uint8
 import cv2
 import numpy as np
 from PIL import Image,ImageDraw,ImageFilter
@@ -9,4 +10,10 @@ b_ch,g_ch,r_ch = cv2.split(image_wo_alpha[:,:,:3])
 alpha_ch = np.full((1087,1600),255)
 
 dst = np.dstack((b_ch,g_ch,r_ch,alpha_ch))
-cv2.imwrite("test.png",dst)
+
+
+cv2.imshow("DEMO",np.array(dst,dtype=np.uint8))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#cv2.imwrite("demo.png",dst)
