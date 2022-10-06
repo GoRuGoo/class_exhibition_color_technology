3ef8710ac6d91f089540e0e2ffda1a58d64a1dfd
GoRuGoo <dachanyr8989@gmail.com>
2022-10-06 11:45:54 +0900

    黒はうまくいってる

--- functions/convert_green_to_white.py
+++ functions/convert_green_to_white.py
@@ -16,12 +16,20 @@ def convert_green_to_white(image_w_alpha: np.ndarray) -> np.ndarray:
     image_wo_alpha, mask = convert_bgra_to_bgr(image_w_alpha, True)
     hsv_image_wo_alpha = cv2.cvtColor(image_wo_alpha, cv2.COLOR_BGR2HSV)
 
-    first_judge = hsv_image_wo_alpha[:, :, 0] > 20
+    first_judge = hsv_image_wo_alpha[:, :, 0] > 40
     # first_judge = ((hsv_image_wo_alpha[:,:,0]>73)&(hsv_image_wo_alpha[:,:,0]<74))&((hsv_image_wo_alpha[:,:,1]>253)&(hsv_image_wo_alpha[:,:,1]<254))&((hsv_image_wo_alpha[:,:,2]>170)&(hsv_image_wo_alpha[:,:,2]<172))
     # second_judge = (hsv_image_wo_alpha[:,:,1]>253)&(hsv_image_wo_alpha[:,:,1]<254)&(hsv_image_wo_alpha[:,:,2]>170)&(hsv_image_wo_alpha[:,:,2]<172)
     # third_judge = (hsv_image_wo_alpha[:,:,2]>170)&(hsv_image_wo_alpha[:,:,2]<172)
 
     hsv_image_wo_alpha[:, :, 0] = np.where(first_judge, 0, hsv_image_wo_alpha[:, :, 0])
+    hsv_image_wo_alpha[:, :, 1] = np.where(
+        image_wo_alpha[:, :, 0] == 0, 255, hsv_image_wo_alpha[:, :, 1]
+    )
+    hsv_image_wo_alpha[:, :, 2] = np.where(
+        image_wo_alpha[:, :, 0] == 0,
+        255 * (image_wo_alpha[:, :, 2] / 255),
+        image_wo_alpha[:, :, 2],
+    )
 
     convert_bgr_image = cv2.cvtColor(hsv_image_wo_alpha, cv2.COLOR_HSV2BGR)
     return convert_bgr_image
