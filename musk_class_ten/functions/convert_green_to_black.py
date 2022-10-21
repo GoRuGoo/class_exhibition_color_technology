75050b5d5ee696af588b54181dedf258d8d34742
GoRuGoo <dachanyr8989@gmail.com>
2022-10-21 17:41:24 +0900

    wip: 自分の画像に合うinRange&塗りつぶし完成しかし色おかしい

--- musk_class_ten/functions/convert_green_to_black.py
+++ musk_class_ten/functions/convert_green_to_black.py
@@ -13,11 +13,22 @@ def convert_green_to_black(image_wo_alpha: np.ndarray) -> np.ndarray:
         _type_:緑色の背景を黒色に置換した画像
     """
     hsv_img = cv2.cvtColor(image_wo_alpha, cv2.COLOR_BGR2HSV)
-    binary_image = cv2.inRange(hsv_img, (62, 100, 0), (79, 255, 255))
+    binary_image = cv2.inRange(hsv_img, (60, 0, 0), (90, 255, 255))
     binary_image = cv2.bitwise_not(binary_image)
+
+    cv2.imwrite("default_binary.png", binary_image)
+
+    countours, hierarchy = cv2.findContours(
+        binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
     # 2値化する
     binary_image = binary_to_bgr_convert(binary_image)
+    output = cv2.drawContours(binary_image, countours, -1, (255, 0, 0), 3)
 
+    cv2.imwrite("binary_rinkaku.png", output)
+    cv2.fillPoly(output, countours, 255)
+
+    cv2.imwrite("fill.png", output)
     transparent = (255, 255, 255)
     result_image = np.where(binary_image == transparent, image_wo_alpha, binary_image)
     return result_image
