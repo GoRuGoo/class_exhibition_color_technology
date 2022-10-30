c656717d2bc20d74d4db9d3d0810bc739c707df0
GoRuGoo <dachanyr8989@gmail.com>
2022-10-31 03:26:25 +0900

    add: HTTPS化するコードを実装

--- fastapi/app/run.py
+++ fastapi/app/run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, host="127.0.0.1", reload=True, access_log=True)
+    uvicorn.run("main:app", port=8000, host="100.64.1.47", reload=True, access_log=True)
