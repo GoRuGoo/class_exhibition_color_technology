a7005d14a234fbe682e233126963b9e7360d90a0
GoRuGoo <dachanyr8989@gmail.com>
2022-11-05 01:30:51 +0900

    test: matplotlibグラフを一旦保存せずにnumpy配列に変換可能

--- musk_class_ten/test_code/functions/visualization_hsv.py
+++ musk_class_ten/test_code/functions/visualization_hsv.py
@@ -1,5 +1,8 @@
+import io
+
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def show_img(path):
@@ -13,10 +16,14 @@ def show_img(path):
     plt.plot(hist_s, color="g", label="s")
     plt.plot(hist_v, color="b", label="v")
     plt.legend()
-    plt.show()
+    # plt.show()
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    enc = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+    dst = cv2.imdecode(enc, 1)
+    cv2.imwrite("test.png", dst)
 
     return hist_h, hist_s, hist_v
 
 
 h, s, v = show_img("komura.png")
-#
