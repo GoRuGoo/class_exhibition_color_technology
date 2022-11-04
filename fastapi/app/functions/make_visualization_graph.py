00fbc91185738a9ef4055e68d1a27fe27f618016
GoRuGoo <dachanyr8989@gmail.com>
2022-11-05 02:54:07 +0900

    add: グラフを毎回更新できるようにした

---
+++ fastapi/app/functions/make_visualization_graph.py
@@ -0,0 +1,26 @@
+import io
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def make_visualization_graph(image: np.ndarray) -> np.ndarray:
+    h, s, v = image[:, :, 0], image[:, :, 1], image[:, :, 2]
+    hist_h = cv2.calcHist([h], [0], None, [256], [0, 256])
+    hist_s = cv2.calcHist([s], [0], None, [256], [0, 256])
+    hist_v = cv2.calcHist([v], [0], None, [256], [0, 256])
+    plt.plot(hist_h, color="r", label="h")
+    plt.plot(hist_s, color="g", label="s")
+    plt.plot(hist_v, color="b", label="v")
+    plt.legend()
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    enc = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+    dst = cv2.imdecode(enc, 1)
+    w, h = dst.shape[:2]
+    dst = cv2.resize(dst, dsize=(656, 496), fx=w / 656, fy=h / 496)
+
+    plt.cla()
+
+    return dst
