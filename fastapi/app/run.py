6114a81d264e9d77f9c17b53da3ff7214456dde6
GoRuGoo <dachanyr8989@gmail.com>
2022-11-04 23:26:33 +0900

    feat: 実行ファイルにSSL証明書のパスを追加＆本番環境に合うパラメーターを調整

--- fastapi/app/run.py
+++ fastapi/app/run.py
@@ -2,5 +2,11 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app", port=8000, host="192.168.10.2", reload=True, access_log=True
+        "main:app",
+        port=8000,
+        host="192.168.10.3",
+        reload=True,
+        access_log=True,
+        ssl_keyfile="./server.key",
+        ssl_certfile="./server.crt",
     )
